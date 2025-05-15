3595f419f6e8eb150d75c80eb17b5da163f4bea7
andrewql01 <114111690+andrewql01@users.noreply.github.com>
2025-05-15 18:27:41 +0200

    initialized swift project

---
+++ mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by Andrzej Wójciak on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobileTests/mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobileTests.swift
+//  mobileTests
+//
+//  Created by Andrzej Wójciak on 15/05/2025.
+//
+
+import Testing
+@testable import mobile
+
+struct mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
